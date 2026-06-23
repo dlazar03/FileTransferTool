88fdaa7aea3ee5d8f90fd63ae49b94dce2267363
Dejana <dejanalazarova@gmail.com>
2026-06-23 13:20:28 +0200

    Initial project setup, file stream ops

---
+++ FileTransferService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileTransferTool
+{
+    public class FileTransferService
+    {
+        public async Task TransferAsync(string sourceFilePath, string destinationFilePath, CancellationToken cancellationToken) {
+            const int chunkSize = 1024 * 1024;
+            var buffer = new byte[chunkSize];
+
+            using var sourceFileStream = 
+                new FileStream(sourceFilePath, FileMode.Open, FileAccess.Read, 
+                FileShare.Read, bufferSize: 4096, useAsync: true);
+
+            using var destinationFileStream = 
+                new FileStream(destinationFilePath, FileMode.Create, FileAccess.ReadWrite, 
+                FileShare.Read, bufferSize:4096, useAsync: true);
+
+            long totalBytes = sourceFileStream.Length;
+            long readCount = 0;
+            int chunkIndex = 0;
+
+            while ((readCount = await sourceFileStream.ReadAsync(buffer, cancellationToken)) > 0)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                long chunkPositionStart = sourceFileStream.Position - readCount;
+                long chunkPositionEnd = sourceFileStream.Position;
+
+                Console.WriteLine($"Current chunk processing: {chunkIndex}");
+                Console.WriteLine($"Chunk start at byte: {chunkPositionStart} end at byte: {chunkPositionEnd}, chunk size: {readCount}");
+
+                await destinationFileStream.WriteAsync(buffer, cancellationToken);
+                chunkIndex++;
+            }
+            Console.WriteLine("Transfer completed.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿
+using FileTransferTool;
+
+try
+{
+    Console.Write("Enter source file path: ");
+    var sourceFilePath = (Console.ReadLine() ?? string.Empty).Trim();
+
+    if (string.IsNullOrWhiteSpace(sourceFilePath)) {
+        throw new ArgumentException("Source file path is required.");
+    }
+
+    if (!File.Exists(sourceFilePath)) {
+        throw new FileNotFoundException($"Source file under {sourceFilePath} could not be found.");
+    }
+
+    Console.Write("Enter destination folder path: ");
+    var destinationFolderPath = (Console.ReadLine() ?? string.Empty).Trim();
+
+    if (string.IsNullOrWhiteSpace(destinationFolderPath)) {
+        throw new ArgumentException("Destination file path is required.");
+    }
+
+    Directory.CreateDirectory(destinationFolderPath);
+    var mergedDestinationPath = Path.Combine(destinationFolderPath, Path.GetFileName(sourceFilePath));
+
+    var fullSourcePath = Path.GetFullPath(sourceFilePath);
+    var fullDestinationPath = Path.GetFullPath(mergedDestinationPath);
+
+    if (string.Equals(fullSourcePath, fullDestinationPath, StringComparison.OrdinalIgnoreCase)) {
+        throw new InvalidOperationException("Source and destination file paths are the same file, provide different path.");
+    }
+
+    var fileService = new FileTransferService();
+    var cancellationToken = new CancellationTokenSource();
+    await fileService.TransferAsync(fullSourcePath, fullDestinationPath, cancellationToken.Token);
+
+    return 0;
+}
+catch (Exception ex) {
+    Console.Error.WriteLine($"Error occured: {ex.Message}");
+    return 1;
+}
